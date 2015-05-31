7997a70bbd74d89ab6f88446d412f6718c430707
Phil Bri <>
2015-05-31 16:54:26 +0200

    v0.1

---
+++ DuinoRemote.js
@@ -0,0 +1,38 @@
+var serialPort = {}, ledState;
+
+exports.action = function(data, next){
+  info('Plugin DuinoRemote is called ...', data);
+  
+  if(data.cmd=='?') {
+    ledState==0 ? data.tts=data.tts.replace('%','éteinte') : data.tts=data.tts.replace('%', 'allumée');
+  }
+
+  serialPort.write(data.cmd, function (err ,byteWritten){
+    if (err) error('DuinoRemote: '+err);
+    info("DuinoRemote: Commande = "+data.cmd+" (Envoi de "+byteWritten+" octets)");
+  });
+
+  serialPort.on('error', function (erreur) {error('DuinoRemote Error: '+erreur)});
+  next({ });
+}
+
+exports.init = function(){
+  var serial = require("serialport"),
+      SerialPort = serial.SerialPort,
+      portName = Config.modules.DuinoRemote.Port;
+
+  serialPort = new SerialPort(portName, {
+    baudrate: 9600,
+    parser: serial.parsers.readline('\n')
+  });
+
+  serialPort.on('data', function (data) {ledState = data});
+  info('Plugin DuinoRemote is initializing ...');
+}
+
+exports.ajax = function(req, res, next){
+  // Called before rendering portlet when clicking on 
+  // <a href="/plugin/duinoremote" data-action="ajax">click me</a>  
+  // Because portlet CAN'T do asynchronous code
+  next();
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# DuinoRemote
+
+Plugin for S.A.R.A.H project by JP Encausse http://blog.encausse.net/s-a-r-a-h/
+
+
+
+## Description
+
+This is a 'demo' module for turning 'ON'/'OFF' and geting the state of an Arduino's LED.
+
+### Quick Start
+
+Donwload the plugin from SARAH's AppStore and follow online plugin's documentation.
+Update Arduino with the given .ino file...
+
+### WARNING
+
+Only Compatible with S.A.R.A.H V4.x !
+
+Dont use with previous versions (v3.1x).
+   
